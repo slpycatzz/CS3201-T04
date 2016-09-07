961b47ddf48c35bf3b1d00d70d4aa2ebf982e931
Tyson Quek Yang Sheng <quektyson@gmail.com>
2016-09-07 22:51:27 +0800

    Update PKB with getters

--- CS3201/SPA/include/PKB/PKB.h
+++ CS3201/SPA/include/PKB/PKB.h
@@ -1,9 +1,10 @@
 #pragma once
 
 #include <string>
-
+#include <vector>
 #include "Constants.h"
 #include "TreeNode.h"
+#include "PKB/Table.h"
 
 class PKB {
  public:
@@ -16,4 +17,53 @@ class PKB {
     static TreeNode* CreateASTNode(Symbol symbol, int lineNumber);
     static TreeNode* CreateASTNode(Symbol symbol, int lineNumber, std::string value);
     static void PrintASTTree();
+
+    static void InsertVariable(std::vector<std::string> variable_names);
+    static bool IsVariableExist(std::string variable_name);
+
+    static void InsertProcedure(std::vector<std::string> procedure_names);
+    static void InsertProcedureLineNumber(std::string procedure_name, std::vector<int> line_numbers);
+    static bool IsProcedureExist(std::string procedure_name);
+
+    static void InsertUses(int line_number, std::vector<std::string> variable_names);
+    static void InsertUses(std::string procedure_name, std::vector<std::string> variable_names);
+    static std::vector<std::string> UsesVariable(int line_number);
+    static std::vector<std::string> UsesVariable(std::string procedure_name);
+    static std::vector<int> UsedByLineNumber(std::string variable_name);
+    static std::vector<std::string> UsedByProcedure(std::string variable_name);
+
+    static void InsertModifies(int line_number, std::vector<std::string> variable_names);
+    static void InsertModifies(std::string procedure_name, std::vector<std::string> variable_names);
+    static std::vector<std::string> ModifiesVariable(int line_number);
+    static std::vector<std::string> ModifiesVariable(std::string procedure_name);
+    static std::vector<int> ModifiedByLineNumber(std::string variable_name);
+    static std::vector<std::string> ModifiedByProcedure(std::string variable_name);
+
+    static void InsertCalls(std::string procedure_name1, std::vector<std::string> procedure_names);
+    static void InsertCallsTransitive(std::string procedure_name1, std::vector<std::string> procedure_names);
+    static bool IsCalls(std::string procedure_name1, std::string procedure_name2);
+    static bool IsCallsTransitive(std::string procedure_name1, std::string procedure_name2);
+    static void InsertParent(int line_number1, std::vector<int> line_numbers);
+    static void InsertParentTransitive(int line_number1, std::vector<int> line_numbers);
+    static bool IsParent(int line_number1, int line_number2);
+    static bool IsParentTransitive(int line_number1, int line_number2);
+    static void InsertFollows(int line_number1, std::vector<int> line_numbers);
+    static void InsertFollowsTransitive(int line_number1, std::vector<int> line_numbers);
+    static bool IsFollows(int line_number1, int line_number2);
+    static bool IsFollowsTransitive(int line_number1, int line_number2);
+
+ private:
+  static std::vector<std::string> variable_table;
+  static std::vector<std::string> procedure_table;
+  static Table<std::string, std::vector<int>> procedure_line_table;
+  static Table<int, std::vector<std::string>> uses_table;
+  static Table<int, std::vector<std::string>> modifies_table;
+  static Table<std::string, std::vector<std::string>> uses_procedure_table;
+  static Table<std::string, std::vector<std::string>> modifies_procedure_table;
+  static Table<std::string, std::vector<std::string>> calls_table;
+  static Table<int, std::vector<int>> parent_table;
+  static Table<int, std::vector<int>> follows_table;
+  static Table<std::string, std::vector<std::string>> calls_transitive_table;
+  static Table<int, std::vector<int>> parent_transitive_table;
+  static Table<int, std::vector<int>> follows_transitive_table;
 };
--- CS3201/SPA/source/PKB/PKB.cpp
+++ CS3201/SPA/source/PKB/PKB.cpp
@@ -1,9 +1,10 @@
 #include <string>
-
+#include <vector>
 #include "Constants.h"
 #include "PKB/AST.h"
 #include "PKB/PKB.h"
 #include "TreeNode.h"
+#include "PKB/Table.h"
 
 PKB::PKB() {}
 
@@ -35,4 +36,134 @@ TreeNode* PKB::CreateASTNode(Symbol symbol, int lineNumber, std::string value) {
 
 void PKB::PrintASTTree() {
     AST::getInstance()->printTree();
+}
+
+void PKB::InsertVariable(std::vector<std::string> variable_names) {
+  PKB::variable_table = variable_names;
+}
+
+bool PKB::IsVariableExist(std::string variable_name) {
+  return false;
+}
+
+void PKB::InsertProcedure(std::vector<std::string> procedure_names) {
+  PKB::procedure_table = procedure_names;
+}
+
+void PKB::InsertProcedureLineNumber(std::string procedure_name, std::vector<int> line_numbers) {
+  PKB::procedure_line_table.Insert(procedure_name, line_numbers);
+}
+
+bool PKB::IsProcedureExist(std::string procedure_name) {
+  return false;
+}
+
+void PKB::InsertUses(int line_number, std::vector<std::string> variable_names) {
+  PKB::uses_table.Insert(line_number, variable_names);
+}
+
+void PKB::InsertUses(std::string procedure_name, std::vector<std::string> variable_names) {
+  PKB::uses_procedure_table.Insert(procedure_name, variable_names);
+}
+
+std::vector<std::string> PKB::UsesVariable(int line_number) {
+  return PKB::uses_table.GetValue(line_number);
+}
+
+std::vector<std::string> PKB::UsesVariable(std::string procedure_name) {
+  return PKB::uses_procedure_table.GetValue(procedure_name);
+}
+
+// TODO(pixelducky): update this function
+std::vector<int> PKB::UsedByLineNumber(std::string variable_name) {
+  //return PKB::uses_table.GetKey(variable_name);
+  return std::vector<int>();
+}
+
+// TODO(pixelducky): update this function
+std::vector<std::string> PKB::UsedByProcedure(std::string variable_name) {
+  //return PKB::uses_procedure_table.GetKey(variable_name);
+  return std::vector<std::string>();
+}
+
+void PKB::InsertModifies(int line_number, std::vector<std::string> variable_names) {
+  PKB::modifies_table.Insert(line_number, variable_names);
+}
+
+void PKB::InsertModifies(std::string procedure_name, std::vector<std::string> variable_names) {
+  PKB::modifies_procedure_table.Insert(procedure_name, variable_names);
+}
+
+std::vector<std::string> PKB::ModifiesVariable(int line_number) {
+  return PKB::modifies_table.GetValue(line_number);
+}
+
+std::vector<std::string> PKB::ModifiesVariable(std::string procedure_name) {
+  return PKB::modifies_procedure_table.GetValue(procedure_name);
+}
+
+// TODO(pixelducky): update this function
+std::vector<int> PKB::ModifiedByLineNumber(std::string variable_name) {
+  //return PKB::modifies_table.GetKey(variable_name);
+  return std::vector<int>();
+}
+
+// TODO(pixelducky): update this function
+std::vector<std::string> PKB::ModifiedByProcedure(std::string variable_name) {
+  //return PKB::modifies_procedure_table.GetKey(variable_name);
+  return std::vector<std::string>();
+}
+
+void PKB::InsertCalls(std::string procedure_name1, std::vector<std::string> procedure_names) {
+  PKB::calls_table.Insert(procedure_name1, procedure_names);
+}
+
+void PKB::InsertCallsTransitive(std::string procedure_name1, std::vector<std::string> procedure_names) {
+  PKB::calls_transitive_table.Insert(procedure_name1, procedure_names);
+}
+
+// TODO(pixelducky): update this function
+bool PKB::IsCalls(std::string procedure_name1, std::string procedure_name2) {
+  return false;
+}
+
+// TODO(pixelducky): update this function
+bool PKB::IsCallsTransitive(std::string procedure_name1, std::string procedure_name2) {
+  return false;
+}
+
+void PKB::InsertParent(int line_number1, std::vector<int> line_numbers) {
+  PKB::parent_table.Insert(line_number1, line_numbers);
+}
+
+void PKB::InsertParentTransitive(int line_number1, std::vector<int> line_numbers) {
+  PKB::parent_transitive_table.Insert(line_number1, line_numbers);
+}
+
+// TODO(pixelducky): update this function
+bool PKB::IsParent(int line_number1, int line_number2) {
+  return false;
+}
+
+// TODO(pixelducky): update this function
+bool PKB::IsParentTransitive(int line_number1, int line_number2) {
+  return false;
+}
+
+void PKB::InsertFollows(int line_number1, std::vector<int> line_numbers) {
+  PKB::follows_table.Insert(line_number1, line_numbers);
+}
+
+void PKB::InsertFollowsTransitive(int line_number1, std::vector<int> line_numbers) {
+  PKB::follows_transitive_table.Insert(line_number1, line_numbers);
+}
+
+// TODO(pixelducky): update this function
+bool PKB::IsFollows(int line_number1, int line_number2) {
+  return false;
+}
+
+// TODO(pixelducky): update this function
+bool PKB::IsFollowsTransitive(int line_number1, int line_number2) {
+  return false;
 }
