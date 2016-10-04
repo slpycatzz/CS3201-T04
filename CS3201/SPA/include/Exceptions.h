851676860419a86f9a7a290065fa7e1318d13f0b
C-YanHua <yanhua_92@hotmail.com>
2016-10-04 08:57:44 +0800

    Cleanup Exceptions.h.

--- CS3201/SPA/include/Exceptions.h
+++ CS3201/SPA/include/Exceptions.h
@@ -8,48 +8,82 @@ const char MESSAGE_FILENOTFOUND[]       = "A file with the specified filepath do
 const char MESSAGE_PROGRAMSYNTAXERROR[] = "Frontend parser encountered a syntax error in the program.";
 const char MESSAGE_PROGRAMLOGICERROR[]  = "Frontend parser encountered a logic error in the program.";
 const char MESSAGE_QUERYSYNTAXERROR[]   = "Query parser encountered a syntax error in the query.";
+const char MESSAGE_QUERYLOGICERROR[]    = "Query parser encountered a logic error in the query.";
 
 class Exception : public std::exception {
  public:
     virtual const char* what() const throw() {
-        return message.c_str();
+        return message_.c_str();
     }
 
     Exception() {
-        message = MESSAGE_DEFAULT;
+        message_ = MESSAGE_DEFAULT;
     }
 
-    std::string message;
+    void setMessage(const char *message) {
+        message_ = message;
+    }
+
+    void setMessageWithReason(const char *message, std::string reason) {
+        message_ = message;
+        message_.append(" : " + reason);
+    }
+
+ private:
+    std::string message_;
 };
 
 class FileNotFoundException : public Exception {
  public:
     FileNotFoundException() {
-        message = MESSAGE_FILENOTFOUND;
+        setMessage(MESSAGE_FILENOTFOUND);
+    }
+
+    FileNotFoundException(std::string reason) {
+        setMessageWithReason(MESSAGE_FILENOTFOUND, reason);
     }
 };
 
 class ProgramSyntaxErrorException : public Exception {
  public:
     ProgramSyntaxErrorException() {
-        message = MESSAGE_PROGRAMSYNTAXERROR;
+        setMessage(MESSAGE_PROGRAMSYNTAXERROR);
+    }
+
+    ProgramSyntaxErrorException(std::string reason) {
+        setMessageWithReason(MESSAGE_PROGRAMSYNTAXERROR, reason);
     }
 };
 
 class ProgramLogicErrorException : public Exception {
  public:
     ProgramLogicErrorException() {
-        message = MESSAGE_PROGRAMLOGICERROR;
+        setMessage(MESSAGE_PROGRAMLOGICERROR);
+    }
+
+    ProgramLogicErrorException(std::string reason) {
+        setMessageWithReason(MESSAGE_PROGRAMLOGICERROR, reason);
     }
 };
 
 class QuerySyntaxErrorException : public Exception {
  public:
     QuerySyntaxErrorException() {
-        message = MESSAGE_QUERYSYNTAXERROR;
+        setMessage(MESSAGE_QUERYSYNTAXERROR);
     }
-    QuerySyntaxErrorException(std::string extraInfo) {
-        message = MESSAGE_QUERYSYNTAXERROR;
-        message += " " + extraInfo;
+
+    QuerySyntaxErrorException(std::string reason) {
+        setMessageWithReason(MESSAGE_QUERYSYNTAXERROR, reason);
+    }
+};
+
+class QueryLogicErrorException : public Exception {
+ public:
+    QueryLogicErrorException() {
+        setMessage(MESSAGE_QUERYLOGICERROR);
+    }
+
+    QueryLogicErrorException(std::string reason) {
+        setMessageWithReason(MESSAGE_QUERYLOGICERROR, reason);
     }
 };
