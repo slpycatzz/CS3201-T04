a248e23c88dfe0c895c0ea8e7ed6b33b56985fae
C-YanHua <yanhua_92@hotmail.com>
2016-10-21 22:51:37 +0800

    Improve the auto test script to check if queries failed in the test cases ran.

--- CS3201/tools/AutoTestScript.bat
+++ CS3201/tools/AutoTestScript.bat
@@ -1,12 +1,31 @@
 @echo off
-REM Running the test cases.
-start /wait ..\Release\AutoTester.exe ..\tests\SystemTesting\iter1-follows-source.txt ..\tests\SystemTesting\iter1-follows-queries.txt ..\tests\SystemTesting\output\iter1-follows.xml
-start /wait ..\Release\AutoTester.exe ..\tests\SystemTesting\iter1-source.txt ..\tests\SystemTesting\iter1-queries.txt ..\tests\SystemTesting\output\iter1.xml
-start /wait ..\Release\AutoTester.exe ..\tests\SystemTesting\1-Source.txt ..\tests\SystemTesting\1-Queries.txt ..\tests\SystemTesting\output\out1.xml
-start /wait ..\Release\AutoTester.exe ..\tests\SystemTesting\2-Source.txt ..\tests\SystemTesting\2-Queries.txt ..\tests\SystemTesting\output\out2.xml
-start /wait ..\Release\AutoTester.exe ..\tests\SystemTesting\3-Source.txt ..\tests\SystemTesting\3-Queries.txt ..\tests\SystemTesting\output\out3.xml
-start /wait ..\Release\AutoTester.exe ..\tests\SystemTesting\4-Source.txt ..\tests\SystemTesting\4-Queries.txt ..\tests\SystemTesting\output\out4.xml
-start /wait ..\Release\AutoTester.exe ..\tests\SystemTesting\5-Source.txt ..\tests\SystemTesting\5-Queries.txt ..\tests\SystemTesting\output\out5.xml
-start /wait ..\Release\AutoTester.exe ..\tests\SystemTesting\6-Source.txt ..\tests\SystemTesting\6-Queries.txt ..\tests\SystemTesting\output\out6.xml
-start /wait ..\Release\AutoTester.exe ..\tests\SystemTesting\7-Source.txt ..\tests\SystemTesting\7-Queries.txt ..\tests\SystemTesting\output\out7.xml
-exit
+
+set runDir=..\Release\
+set testDir=..\tests\SystemTesting\
+set error="<failed>"
+
+rem set target test directory
+if not [%1]==[] set testDir=%1
+
+start /wait %runDir%AutoTester.exe %testDir%iter1-follows-source.txt %testDir%iter1-follows-queries.txt %testDir%output\iter1-follows.xml
+rem start /wait %runDir%AutoTester.exe %testDir%iter1-source.txt %testDir%iter1-queries.txt %testDir%output\iter1.xml
+start /wait %runDir%AutoTester.exe %testDir%1-Source.txt %testDir%1-Queries.txt %testDir%output\out1.xml
+start /wait %runDir%AutoTester.exe %testDir%2-Source.txt %testDir%2-Queries.txt %testDir%output\out2.xml
+start /wait %runDir%AutoTester.exe %testDir%3-Source.txt %testDir%3-Queries.txt %testDir%output\out3.xml
+start /wait %runDir%AutoTester.exe %testDir%4-Source.txt %testDir%4-Queries.txt %testDir%output\out4.xml
+start /wait %runDir%AutoTester.exe %testDir%5-Source.txt %testDir%5-Queries.txt %testDir%output\out5.xml
+start /wait %runDir%AutoTester.exe %testDir%6-Source.txt %testDir%6-Queries.txt %testDir%output\out6.xml
+start /wait %runDir%AutoTester.exe %testDir%7-Source.txt %testDir%7-Queries.txt %testDir%output\out7.xml
+
+setlocal enabledelayedexpansion
+
+rem check if there is any error occurred.
+for %%f in (%testDir%output\*.xml) do (
+    rem output filename that have failed test cases.
+    >nul findstr /C:%error% %%f && (
+      echo Queries failed in %%~nf.xml
+    )
+)
+
+echo All test cases completed^^!^^!
+pause
