207e3b27a51984301c65261e23fdb56909b923be
Bevin Seetoh Jia Jin <a0134234@u.nus.edu>
2016-10-24 12:31:06 +0800

    More test cases

--- CS3201/tools/AutoTestScript.bat
+++ CS3201/tools/AutoTestScript.bat
@@ -20,11 +20,32 @@ start /wait %runDir%AutoTester.exe %testDir%8-Source.txt %testDir%8-Queries.txt 
 start /wait %runDir%AutoTester.exe %testDir%9-Source.txt %testDir%9-Queries.txt %testDir%output\out9.xml
 start /wait %runDir%AutoTester.exe %testDir%10-Source.txt %testDir%10-Queries.txt %testDir%output\out10.xml
 start /wait %runDir%AutoTester.exe %testDir%11-Source.txt %testDir%11-Queries.txt %testDir%output\out11.xml
+start /wait %runDir%AutoTester.exe %testDir%clauseTesting\SourceOne.txt %testDir%clauseTesting\Calls.txt %testDir%clauseTesting\output\Calls.xml
+start /wait %runDir%AutoTester.exe %testDir%clauseTesting\SourceOne.txt %testDir%clauseTesting\CallsStar.txt %testDir%clauseTesting\output\CallsStar.xml
+start /wait %runDir%AutoTester.exe %testDir%clauseTesting\SourceOne.txt %testDir%clauseTesting\Custom.txt %testDir%clauseTesting\output\Custom.xml
+start /wait %runDir%AutoTester.exe %testDir%clauseTesting\SourceOne.txt %testDir%clauseTesting\Follows.txt %testDir%clauseTesting\output\Follows.xml
+start /wait %runDir%AutoTester.exe %testDir%clauseTesting\SourceOne.txt %testDir%clauseTesting\FollowsStar.txt %testDir%clauseTesting\output\FollowsStar.xml
+start /wait %runDir%AutoTester.exe %testDir%clauseTesting\SourceOne.txt %testDir%clauseTesting\Modifies.txt %testDir%clauseTesting\output\Modifies.xml
+start /wait %runDir%AutoTester.exe %testDir%clauseTesting\SourceOne.txt %testDir%clauseTesting\Next.txt %testDir%clauseTesting\output\Next.xml
+start /wait %runDir%AutoTester.exe %testDir%clauseTesting\SourceOne.txt %testDir%clauseTesting\NextStar.txt %testDir%clauseTesting\output\NextStar.xml
+start /wait %runDir%AutoTester.exe %testDir%clauseTesting\SourceOne.txt %testDir%clauseTesting\Parent.txt %testDir%clauseTesting\output\Parent.xml
+start /wait %runDir%AutoTester.exe %testDir%clauseTesting\SourceOne.txt %testDir%clauseTesting\ParentStar.txt %testDir%clauseTesting\output\ParentStar.xml
+start /wait %runDir%AutoTester.exe %testDir%clauseTesting\SourceOne.txt %testDir%clauseTesting\Pattern.txt %testDir%clauseTesting\output\Pattern.xml
+start /wait %runDir%AutoTester.exe %testDir%clauseTesting\SourceOne.txt %testDir%clauseTesting\Uses.txt %testDir%clauseTesting\output\Uses.xml
 
 setlocal enabledelayedexpansion
 
 rem check if there is any error occurred.
 for %%f in (%testDir%output\*.xml) do (
+    rem output filename that have failed test cases.
+    >nul findstr /C:%error% %%f && (
+        echo Queries failed in %%~nf.xml
+    ) || (
+        echo All queries passed for %%~nf.xml
+    )
+)
+
+for %%f in (%testDir%clauseTesting\output\*.xml) do (
     rem output filename that have failed test cases.
     >nul findstr /C:%error% %%f && (
         echo Queries failed in %%~nf.xml
